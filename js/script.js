81a552f6a1618cde3c6bcc296ccb7c9cf72ca274
FloresZyra <flores.zyra0605@gmail.com>
2026-02-19 12:13:41 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 console.log("js/script.js")
 
-const submitBtn = document.getElementById("submitBtn");
-
-submitBtn.addEventListener("click",function(event){
-    event.preventDefault();
-    alert("Thank you! Your message has been received.");
-});
-
 
 const themeToggle = document.getElementById("themeToggle");
 
@@ -22,6 +15,7 @@ toggleSkills.addEventListener("click",function(){
     skillsSection.classList.toggle("hidden");
 });
 
+const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click",function(event){
     event.preventDefault();
     const name = document.getElementById("name").value;
@@ -30,5 +24,7 @@ submitBtn.addEventListener("click",function(event){
         alert("Please fill in all required fields.");
     } else{
         alert("Form submitted successfully!");
+        alert("Thank you! Your message has been received.")
     }
 });
+
